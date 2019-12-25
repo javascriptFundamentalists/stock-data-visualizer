fab1994db0c44a5aba647d61ce7115509049e3f4
Brian Ha <brianha32@gmail.com>
2019-12-24 22:14:11 -0500

    Tried working on getCHRISData of quandl.js file.

--- src/quandl/quandl.js
+++ src/quandl/quandl.js
@@ -23,4 +23,7 @@ export const getBATSData = (code) => {
  */
 export const getCHRISData = (code) => {
   // TODO: Brian complete this API call
+  const url = `https://www.quandl.com/api/v3/datasets/CHRIS/${code}.json?api_key=${API_KEY}`;
+  const dataPromise = axios.get(url);
+  return dataPromise;
 }
