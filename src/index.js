3cf1a593b700b130eabeda05b15b34cb3dd68a4c
Brian Ha <brianha32@gmail.com>
2020-01-06 15:06:52 -0500

    Made some changes to index.js file.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 // Including other files with ES6 modules
-//
 import "core-js/stable";
 import "regenerator-runtime/runtime";
 
@@ -11,10 +10,6 @@ import { SideBarComponent } from "./components/Sidebar";
 import { readBATSmetadata } from "./d3/csv";
 import { readCHRISmetadata } from "./d3/csv";
 
-// limit the number of tickers for now
-const limit = 15;
-let counter = 0;
-
 // available data sources
 const sources = [
   {key: 'bats', name: 'BATS Exchange Equities'},
@@ -22,17 +17,8 @@ const sources = [
 ];
 
 (async () => {
-  // const moredata = await readBATSmetadata();
   const data = await readCHRISmetadata();
   const codes = [];
-  while (counter < limit) {
-    // For BATS data
-    // codes.push({ key: moredata[counter].code, name: moredata[counter].code });
-    // For CHRIS data
-    const symbol = `${data[counter].Exchange}_${data[counter].Ticker}1`;
-    codes.push({ key: symbol, name: symbol });
-    counter++;
-  }
 
   const app = new AppComponent({ tickers: codes, sources: sources }, "root", [
     [new SideBarComponent({}, null, []), "sidebar"],
